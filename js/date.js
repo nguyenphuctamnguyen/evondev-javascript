d4d02ea576822a5210c69930de9aff61f0cb6002
nguyenngpt <nguyen.ngpt@kyanon.digital>
2023-04-24 04:23:24 +0700

    Các hàm khác trong Date

--- js/date.js
+++ js/date.js
@@ -32,6 +32,7 @@ console.log(birthday.getDate()); //22
 console.log(birthday.getDay()); //6 => thứ bảy6 (0:chủ nhật - 6:thứ bảy)
 console.log(birthday.getHours()); //0
 console.log(birthday.getMinutes()); //0
+console.log(birthday.getSeconds()); //0
 console.log(birthday.getMilliseconds()); //0
 console.log(birthday.getTime());
 
@@ -40,8 +41,17 @@ console.log(`My birthday: ${birthday}`);
 birthday.setFullYear(2001);
 birthday.setMonth(8);
 birthday.setDate(30);
+birthday.setHours(23);
+birthday.setMinutes(23);
+birthday.setSeconds(23);
 console.log(`My birthday after update: ${birthday}`);
+
+// UTC
+console.log(birthday.getUTCFullYear());
 
 //
 console.log(now.toDateString()); //Sun Jun 26 2022
 console.log(now.toTimeString()); //21:20:20 GMT+0700 (Giờ Đông Dương)
+console.log(now.toLocaleDateString()); //m/dd/yyyy -> 6/26/2022
+console.log(now.toLocaleDateString("vi-VI")); // 26/6/2022
+console.log(now.toISOString()); //2023-04-23T21:22:21.536Z
