226d0390c565293d4e822167e9fecbb671678fb5
nguyenngpt <nptn227@gmail.com>
2023-04-27 03:15:03 +0700

    Bài tập về mảng số 2

--- js/array-exercise.js
+++ js/array-exercise.js
@@ -9,3 +9,22 @@ function reverseString(str) {
     return str.split(" ").reverse().join(" ");
 }
 console.log(reverseString("My name is Su")); // Su is name My
+
+// 2. Đảo ngược 1 chuỗi bao gồm các kí tự. Vd: "My name is Su" -> "uS si eman yM"
+function reverseString2(str) {
+    if (!str) return null;
+
+    // const arr = str.split(" "); // ['My', 'name', 'is', 'Su']
+    // const reverseArr = arr.reverse(); // ['Su', 'is', 'name', 'My']
+    // const newReverseArr = reverseArr.map((item) =>
+    //     item.split("").reverse().join("")
+    // );
+    // return newReverseArr.join(" "); // uS si eman yM
+
+    return (arrStr = str
+        .split(" ")
+        .map((item) => item.split("").reverse().join(""))
+        .reverse()
+        .join(" "));
+}
+console.log(reverseString2("My name is Su")); // uS si eman yM
