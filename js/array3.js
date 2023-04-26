a9f1db2432c6a569d3d7befa27b3eb8b6f92753c
nguyenngpt <nptn227@gmail.com>
2023-04-27 04:03:48 +0700

    Cách so sánh 2 mảng cơ bản

--- js/array3.js
+++ js/array3.js
@@ -8,3 +8,25 @@ console.log(num1 === num2); // true
 const arr1 = [1, 2];
 const arr2 = [1, 2];
 console.log(arr1 === arr2); // false
+
+// JSON: Javascript Object Notation
+// JSON.stringify(value) -> convert giá trị sang dưới dạng JSON string
+/**
+ * {
+ *      "key": value,
+ *      "key": value,
+ *      "key": value,
+ *      "key": value,
+ * }
+ */
+// .toString()
+// [1,2,3].toString() -> "1,2,3"
+
+// JSON.stringify([1,2,3]) -> "[1,2,3]"
+// JSON.parse("[1,2,3]") -> [1,2,3]
+console.log(JSON.stringify([1, 2, 3]));
+console.log(JSON.parse("[1,2,3]"));
+
+const arr1Str = JSON.stringify(arr1);
+const arr2Str = JSON.stringify(arr2);
+console.log(arr1Str === arr2Str); // true
