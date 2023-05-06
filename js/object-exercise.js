08a110c6a5af5ed2ec9ca30b8f8df0be34637e89
nguyenngpt <nptn227@gmail.com>
2023-05-07 04:50:37 +0700

    Bài tập chương 7 số 2

--- js/object-exercise.js
+++ js/object-exercise.js
@@ -8,3 +8,24 @@ function isObject(value) {
 console.log(isObject({})); // true
 console.log(isObject([])); // false
 console.log(isObject(null)); // false
+
+// 2. Viết function trả về key value
+// {a: 1, b: 2} -> [["a", 1], ["b", 2]]
+function objectToArray(obj) {
+    if (!isObject(obj)) return;
+
+    // return Object.entries(obj);
+
+    // const value = Object.keys(obj).map((key) => [key, obj[key]]);
+    // return value;
+
+    let result = [];
+    for (let key in obj) {
+        // hasOwnProperty(key) -> nếu object chứa key trả về true, ngược lại trả về false
+        if (obj.hasOwnProperty(key)) {
+            result.push([key, obj[key]]);
+        }
+    }
+    return result;
+}
+console.log(objectToArray({ a: 1, b: 2 })); // [["a", 1], ["b", 2]]
