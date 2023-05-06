2140dc9d25524cb03a0ae5a6b027cb6c9013d8d2
nguyenngpt <nptn227@gmail.com>
2023-05-07 05:44:57 +0700

    document.querySelectorAll

--- dom/dom.js
+++ dom/dom.js
@@ -12,3 +12,9 @@ console.log(singleNode); // <h1>this is title</h1>
 console.log(singleNode2); // <div class="container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores fugiat soluta est animi ad nam enim et culpa veritatis?</div>
 console.log(singleNode3); // <div id="spinner"></div>
 console.log(singleNode4); // null
+
+// document.querySelectorAll('selector'): trả về 1 NodeList chứa danh sách các node, ngược lại trả về empty
+// có thể loop: forEach, map, filter
+// giống array có thể loop nhưng ko sử dụng đc các phương thức như push, pop, shift, ..
+const multiNodes = document.querySelectorAll(".item");
+console.log(multiNodes); // NodeList(5) [li.item, li.item, li.item, li.item, li.item]
