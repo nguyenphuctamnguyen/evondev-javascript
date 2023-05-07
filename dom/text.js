a1418cd601d5298b17c3f8588d2131ccd4562e22
nguyenngpt <nptn227@gmail.com>
2023-05-08 03:35:12 +0700

    Thuộc tính innerText

--- dom/text.js
+++ dom/text.js
@@ -1,7 +1,12 @@
 // textContent
 const spinner = document.querySelector("#spinner");
-console.log(spinner.textContent); // Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores fugiat soluta est animi ad nam enim et culpa veritatis?
+console.log(spinner.textContent); // Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+// asperiores fugiat hello world
+// soluta est animi ad nam enim et culpa veritatis?
 
 // Thay đổi text content
-spinner.textContent = "Hello new content";
-console.log(spinner.textContent); // Hello new content
+// spinner.textContent = "Hello new content";
+// console.log(spinner.textContent); // Hello new content
+
+// innerText
+console.log(spinner.innerText); // Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores fugiat soluta est animi ad nam enim et culpa veritatis?
