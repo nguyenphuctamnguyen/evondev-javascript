b0ac44456e8cc7f17e278cd95bfad9be1895b56f
nguyenngpt <nptn227@gmail.com>
2023-05-09 03:51:21 +0700

    Thực hành phương thức document.appendChild

--- dom/crud.js
+++ dom/crud.js
@@ -7,3 +7,22 @@ const div = document.createElement("div");
 // document.querySelector("body")
 const body = document.body;
 body.appendChild(div);
+
+div.classList.add("container");
+div.className = "container wrapper";
+div.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti assumenda neque vitae quod optio sequi non veritatis labore natus officia.";
+div.innerHTML = `<div class="content"><h3></h3></div>`;
+div.setAttribute("data-name", "evondev");
+
+// Bài tập tạo ra html như đã minh họa ở file index.html
+const card = document.createElement("div");
+card.classList.add("card");
+
+const cardImage = document.createElement("img");
+cardImage.setAttribute("src", "https://source.unsplash.com/random");
+cardImage.classList.add("card-image");
+
+card.appendChild(cardImage);
+
+body.appendChild(card);
