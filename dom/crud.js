a0bc9f68320ee9aba3717e9cf43e719251dcf104
nguyenngpt <nptn227@gmail.com>
2023-05-09 03:28:33 +0700

    document.createElement và document.appendChild

---
+++ dom/crud.js
@@ -0,0 +1,9 @@
+// Thêm xóa sửa node trong Javascript
+// 1. document.createElement("tag"): tạo ra element
+const div = document.createElement("div");
+
+// 2. selector.appendChild
+// document.body: thẻ body
+// document.querySelector("body")
+const body = document.body;
+body.appendChild(div);
