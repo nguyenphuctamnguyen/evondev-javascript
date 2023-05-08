940a3cee29b4e536ee30a163d678101093d330cc
nguyenngpt <nptn227@gmail.com>
2023-05-09 04:31:50 +0700

    Phương thức insertAdjacentElement

--- dom/insert.js
+++ dom/insert.js
@@ -5,3 +5,8 @@ h3.insertAdjacentText("beforebegin", "beforebegin");
 h3.insertAdjacentText("afterbegin", "afterbegin");
 h3.insertAdjacentText("beforeend", "beforeend");
 h3.insertAdjacentText("afterend", "afterend");
+
+// 2. insertAdjacentElement("position",node)
+const strong = document.createElement("strong");
+strong.classList.add("bold");
+h3.insertAdjacentElement("beforeend", strong);
