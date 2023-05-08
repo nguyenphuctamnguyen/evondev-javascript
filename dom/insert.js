5af59a0a0545aca2147260268f9a3376122acc52
nguyenngpt <nptn227@gmail.com>
2023-05-09 04:19:00 +0700

    Phương thức insertAdjacentText (fixed)

--- dom/insert.js
+++ dom/insert.js
@@ -1,8 +1,7 @@
 // 1. insertAdjacentText("position","text")
 // position: beforebegin, afterbegin, beforeend, afterend
 const h3 = document.querySelector("h3");
-// h3.insertAdjacentElement("beforebegin", "begin");
-h3.insertAdjacentHTML("beforebegin", "begin");
-h3.insertAdjacentHTML("afterbegin", "afterbegin");
-h3.insertAdjacentHTML("beforeend", "beforeend");
-h3.insertAdjacentHTML("afterend", "afterend");
+h3.insertAdjacentText("beforebegin", "beforebegin");
+h3.insertAdjacentText("afterbegin", "afterbegin");
+h3.insertAdjacentText("beforeend", "beforeend");
+h3.insertAdjacentText("afterend", "afterend");
