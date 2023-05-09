5fbd23446372ba39a913e572a0a142fc9b97a306
nguyenngpt <nptn227@gmail.com>
2023-05-09 08:37:30 +0700

    Các thuộc tính liên quan tới client

--- dom/sizing.js
+++ dom/sizing.js
@@ -5,3 +5,9 @@ console.log(boxed.offsetHeight); // chiều cao của phần tử: 200
 console.log(boxed.offsetLeft); // vị trí của nó so với bên trái: 0
 console.log(boxed.offsetTop); // vị trí của nó so với bên trên: 0
 console.log(boxed.offsetParent); // lấy ra phần tử cha: body
+
+// 2. clientWidth, clientHeight, clientLeft, clientTop
+console.log(boxed.clientWidth); // độ rộng của phần tử trừ border: 190
+console.log(boxed.clientHeight); // chiều cao của phần tử trừ border: 190
+console.log(boxed.clientLeft); // vị trí của nó so với border bên trái: 5
+console.log(boxed.clientTop); // vị trí của nó so với border bên trên: 5
