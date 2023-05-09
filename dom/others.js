43270ed6ec505402b3382a5c41c60fe415a0d6d6
nguyenngpt <nptn227@gmail.com>
2023-05-09 09:17:36 +0700

    Tìm hiểu document.title và document.head

---
+++ dom/others.js
@@ -0,0 +1,16 @@
+// document.title: thẻ title của trang web
+console.log(document.title); // Javascript
+document.title = "Welcome Ying";
+
+// document.head: thẻ head của trang web
+console.log(document.head); // <head></head>
+// <meta name="viewport" content="width=device-width, initial-scale=1.0">
+const meta = document.createElement("meta");
+meta.setAttribute("name", "viewport");
+meta.setAttribute("content", "width=device-width, initial-scale=1.0");
+document.head.appendChild(meta);
+
+// C2
+const head = document.getElementsByTagName("head");
+head[0].appendChild(meta);
+console.log(head);
