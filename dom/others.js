3759d62a3a94466e5db83a44d1973b8003bc9323
nguyenngpt <nptn227@gmail.com>
2023-05-10 02:57:22 +0700

    Tìm hiểu thêm phương thức replaceChild

--- dom/others.js
+++ dom/others.js
@@ -21,3 +21,21 @@ const ul = document.querySelector("ul");
 document.body.insertBefore(ul, document.querySelector("h3").nextElementSibling);
 // insertAdjacentElement
 // document.querySelector("h3").insertAdjacentElement("beforebegin", ul);
+
+// replaceChild
+// node.replaceChild(newnode, oldnode)
+const span = document.createElement("span");
+span.textContent = "abc";
+document.body.replaceChild(span, document.querySelector(".boxed"));
+
+// convert HTMLCollection, NodeList to Array
+const li = document.getElementsByTagName("li");
+console.log(li); // HTMLCollection(5)
+// Array.from(HTMLCollection or NodeList)
+// [...HTMLCollection], [...NodeList]
+[...li].forEach((item) => item);
+
+// html: document.documentElement
+// body: document.body
+// head: document.head
+// title: document.title
