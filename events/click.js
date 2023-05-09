df9129c2fdd6103c413051f7a98241e4396b7083
nguyenngpt <nptn227@gmail.com>
2023-05-10 03:17:57 +0700

    Bubbling trong click

--- events/click.js
+++ events/click.js
@@ -1,10 +1,23 @@
 // addEventListener
 const button = document.querySelector(".button");
+const span = document.querySelector(".button span");
 function handleClick() {
-    console.log("click me");
+    console.log("click button");
 }
+
 // Lỗi sai khi dùng function
 // button.addEventListener("click", handleClick());
 // Đúng
-button.addEventListener("click", function () {});
+// button.addEventListener("click", function () {});
 button.addEventListener("click", handleClick);
+// event: e
+span.addEventListener("click", function (e) {
+    e.stopPropagation();
+    console.log("click span");
+});
+document.body.addEventListener("click", function () {
+    console.log("click body");
+});
+
+// bubbling: nổi bọt
+// sự kiện click chạy từ trong ra ngoài
