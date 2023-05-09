c7168aab01e2d4c6f631786e69e6803a6f920b4c
nguyenngpt <nptn227@gmail.com>
2023-05-09 08:58:51 +0700

    Tìm hiểu getBoundingClientRect()

--- dom/sizing.js
+++ dom/sizing.js
@@ -17,3 +17,10 @@ console.log(window.innerWidth); // 714
 console.log(window.outerWidth); // 784
 console.log(window.innerHeight); // 720
 console.log(window.outerHeight); // 840
+
+// 4. selector.getBoundingClientRect(): lấy ra tọa độ, kích thước của selector
+console.log(boxed.getBoundingClientRect()); // {x: 0, y: 0, width: 200, height: 200, top: 0, bottom: 0, right: 0,left: 0,}
+// left, x: vị trí của khối so với bên trái
+// top, y: vị trí của khối so với bên trên
+// bottom: chiều cao của khối + top
+// right: độ rộng của khổi + left
