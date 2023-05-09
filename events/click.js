1d2114886e87220b6f644c7a42707c5c715f7c32
nguyenngpt <nptn227@gmail.com>
2023-05-10 03:44:09 +0700

    Sự khác nhau giữa target và currentTarget

--- events/click.js
+++ events/click.js
@@ -9,9 +9,7 @@ function handleClick() {
 // button.addEventListener("click", handleClick());
 // Đúng
 // button.addEventListener("click", function () {});
-button.addEventListener("click", handleClick, {
-    capture: true,
-});
+
 // event: e
 // span.addEventListener(
 //     "click",
@@ -24,16 +22,25 @@ button.addEventListener("click", handleClick, {
 //         capture: true,
 //     }
 // );
-span.addEventListener("click", function (e) {
-    e.stopPropagation();
-    console.log("click span 2");
-});
-document.body.addEventListener("click", function () {
-    console.log("click body");
-});
+// span.addEventListener("click", function (e) {
+//     e.stopPropagation();
+//     console.log("click span 2");
+// });
+// document.body.addEventListener("click", function () {
+//     console.log("click body");
+// });
 
 // bubbling: nổi bọt
 // sự kiện click chạy từ trong ra ngoài
 
 // capturing: nổi bọt
 // sự kiện click chạy từ ngoài vào trong
+
+// Sự khác nhau giữa target và currentTarget
+button.addEventListener("click", function (e) {
+    // event.target: chọn chính xác element mà mình click tới
+    console.log(`e.target: ${e.target}`);
+
+    // event.currentTarget: chọn phần tử mà mình click
+    console.log(`e.currentTarget: ${e.currentTarget}`);
+});
