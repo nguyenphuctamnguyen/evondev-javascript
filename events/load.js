1f32ee9ac3e13843e47b54419b8b54bfd356bf88
nguyenngpt <nptn227@gmail.com>
2023-05-12 06:28:36 +0700

    Sự khác nhau giữa load và DOMContentLoaded

---
+++ events/load.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded")
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded");
+});
+
+// window.addEventListener("load")
+// web fully loaded
+window.addEventListener("load", function () {
+    console.log("Your web loaded");
+});
