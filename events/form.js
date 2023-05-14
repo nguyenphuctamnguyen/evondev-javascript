37ab0921e56e2accccd199c9ba49b69a2f20524c
nguyenngpt <nptn227@gmail.com>
2023-05-14 21:30:44 +0700

    Sự kiện keyup

--- events/form.js
+++ events/form.js
@@ -1,8 +1,19 @@
-// 1. keydown: hoạt động khi nhấn xuống
+// 1. keydown: sự kiện xảy ra khi nhấn phím
 const input = document.querySelector(".input");
 input.addEventListener("keydown", function (e) {
-    console.log(e.key);
-    console.log(e.keyCode);
-    console.log(e.which);
+    // console.log(e.key);
+    // console.log(e.keyCode);
+    // console.log(e.which);
     // e.key: key nhập vào
+
+    if (e.key === "Enter") {
+        console.log("You typed Enter!");
+    } else {
+        console.log(e.key);
+    }
+});
+
+// 2. keyup: sự kiện xảy ra khi nhấn phím rồi thả ra
+input.addEventListener("keyup", function (e) {
+    console.log(e.key);
 });
