a2dc12c0b3e8fd5a1131d221c004ade5ae489832
nguyenngpt <nptn227@gmail.com>
2023-05-14 21:12:51 +0700

    Sự kiện keydown

---
+++ events/form.js
@@ -0,0 +1,8 @@
+// 1. keydown: hoạt động khi nhấn xuống
+const input = document.querySelector(".input");
+input.addEventListener("keydown", function (e) {
+    console.log(e.key);
+    console.log(e.keyCode);
+    console.log(e.which);
+    // e.key: key nhập vào
+});
