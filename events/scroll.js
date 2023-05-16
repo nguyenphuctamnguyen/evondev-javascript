a54e5aa03d19311a7b3a4f0fa877cd385bba16a4
nguyenngpt <nptn227@gmail.com>
2023-05-16 07:35:54 +0700

    Debounce

--- events/scroll.js
+++ events/scroll.js
@@ -1,4 +1,24 @@
 // 1. Sự kiện scroll
-window.addEventListener("scroll", function (e) {
-    console.log("scroll");
-});
+window.addEventListener(
+    "scroll",
+    debounceFn(function (e) {
+        console.log("scroll");
+    }, 25)
+);
+
+// 2. Debounce
+function debounceFn(func, wait, immediate) {
+    let timeout;
+    return function () {
+        let context = this,
+            args = arguments;
+        let later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
