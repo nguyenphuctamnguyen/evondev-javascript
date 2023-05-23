833b65abd185ed6081fe506206123712d6d90ea2
nguyenngpt <nptn227@gmail.com>
2023-05-23 14:32:11 +0700

    Ranges

--- regular-expression/regex.js
+++ regular-expression/regex.js
@@ -11,7 +11,25 @@ console.log(re1.test("world")); // false
 // ^ string bắt đầu với từ nào đó
 // $ string kết thúc với từ nào đó
 /hi/.test("hi"); // true
+
 /^hi/.test("hi welcome to hi"); // true
 /^hi/.test("welcome to hi"); // true
+
 /hi$/.test("welcome to hi"); // true
 /hi$/.test("welcome to hello"); // false
+
+// 3. Ranges []
+// [a-z]: các kí tự từ a đến z in thường
+// [A-Z]: các kí tự từ A đến Z in hoa
+// [0-9]: các số từ 0 đến 9
+// [a-z0-9A-Z]: các số từ 0-9 hoặc từ a-z hoặc từ A-Z
+/[a-z]/.test("asdf"); // true
+/[a-z]/.test("A"); // false
+
+/[A-Z]/.test("A"); // true
+/[A-Z]/.test("a"); // false
+
+/[0-9]/.test("1000"); // true
+/[0-9]/.test("abc1000abc"); // true
+
+/[a-z0-9A-Z]/.test("123abcAbc"); // true
