6422731cf01c45af34701b8de23b007cab00e92b
nguyenngpt <nptn227@gmail.com>
2023-05-24 05:52:14 +0700

    Tìm hiểu removeEventListener

--- events/other.js
+++ events/other.js
@@ -1,5 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("loaded");
+// document.addEventListener("DOMContentLoaded", function () {
+//     console.log("loaded");
+// });
+const button = document.querySelector(".button");
+
+// removeEventListener
+function handleMouseMove(e) {
+    console.log(e.clientX);
+}
+
+document.addEventListener("mousemove", handleMouseMove);
+
+button.addEventListener("click", function () {
+    document.removeEventListener("mousemove", handleMouseMove);
 });
-const p = document.querySelector("p");
-console.log(p);
