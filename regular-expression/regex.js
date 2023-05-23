9fcb5758a3daca2e933a4617b6885d067e5211f0
nguyenngpt <nptn227@gmail.com>
2023-05-23 14:23:03 +0700

    Anchor

--- regular-expression/regex.js
+++ regular-expression/regex.js
@@ -6,3 +6,12 @@ const re2 = new RegExp("hello");
 // regex.test(value) -> true/false
 console.log(re1.test("hello world")); // true
 console.log(re1.test("world")); // false
+
+// 2. Anchor ^ $
+// ^ string bắt đầu với từ nào đó
+// $ string kết thúc với từ nào đó
+/hi/.test("hi"); // true
+/^hi/.test("hi welcome to hi"); // true
+/^hi/.test("welcome to hi"); // true
+/hi$/.test("welcome to hi"); // true
+/hi$/.test("welcome to hello"); // false
