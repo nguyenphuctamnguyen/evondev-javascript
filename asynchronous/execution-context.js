6e0ebadd6cd64ea5b7c7acd480826ec1b882f6c5
nguyenngpt <nptn227@gmail.com>
2023-06-01 09:31:10 +0700

    Function execution context

--- asynchronous/execution-context.js
+++ asynchronous/execution-context.js
@@ -20,5 +20,19 @@ console.log(y); // 100
  * this: window
  * x: 10
  * timesTen: function(){}
- * y: timesTen(x)
+ * y: timesTen(x) -> 100
+ */
+
+//- Function execution context
+// the creation phase
+/**
+ * global object: arguments
+ * this: window
+ * a: undefined
+ */
+// the execution phase
+/**
+ * global object: arguments
+ * this: window
+ * a: 10
  */
