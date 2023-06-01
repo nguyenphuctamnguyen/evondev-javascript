135fe1b5e46d9438d4fa5eb9b1ae026b65e46668
nguyenngpt <nptn227@gmail.com>
2023-06-01 09:25:57 +0700

    Global execution context

---
+++ asynchronous/execution-context.js
@@ -0,0 +1,24 @@
+let x = 10;
+function timesTen(a) {
+    return a * 10;
+}
+let y = timesTen(x);
+console.log(y); // 100
+
+//- Global execution context
+// the creation phase
+/**
+ * global object: window
+ * this: window
+ * x: undefined
+ * timesTen: function(){}
+ * y: undefined
+ */
+// the execution phase
+/**
+ * global object: window
+ * this: window
+ * x: 10
+ * timesTen: function(){}
+ * y: timesTen(x)
+ */
