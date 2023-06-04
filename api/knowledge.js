f64daf14fc064bff4f3f3d3d1e5c91f08382e014
nguyenphuctamnguyen <nptn227@gmail.com>
2023-06-05 04:32:11 +0700

    Các trạng thái hay gặp khi gọi API

--- api/knowledge.js
+++ api/knowledge.js
@@ -2,3 +2,21 @@
 
 // same origin: evondev.com evondev.com/posts
 // not same origin: evondev.com facebook.com
+
+// 200: Success
+// 201: Create
+
+// 3xx: Redirect
+// 301: Moved Permanently
+// 307: Temporary Redirect
+
+// 4xx: Error
+// 400: Bad Request
+// 401: Unauthorized
+// 403: Forbidden
+// 404: Not Found
+// 408: Request Timeout
+
+// 5xx: Server
+// 502: Bad Gateway
+// 504: Gateway Timeout
