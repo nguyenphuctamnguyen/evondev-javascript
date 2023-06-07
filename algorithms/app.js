d81f9a2686647b55c4d5ca05104d4c285b68a376
nguyenphuctamnguyen <nptn227@gmail.com>
2023-06-07 09:42:23 +0700

    BigO Notation

---
+++ algorithms/app.js
@@ -0,0 +1,13 @@
+// 1. Big O Notation: độ phức tạp của thuật toán
+// O(1): [].push(item): best
+// O(n): for: good
+// O(n^2): nested loop: ok
+// O(logn): for([].push(item)): bad
+// O(n!): 6!: terrible
+function factorial(n) {
+    let num = n;
+    for (let index = 0; index < n; index++) {
+        num = factorial(n - 1);
+    }
+    return;
+}
