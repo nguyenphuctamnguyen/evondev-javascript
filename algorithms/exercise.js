87afd92501a4b5a832ff97654d6acf6304101117
nguyenphuctamnguyen <nptn227@gmail.com>
2023-09-12 04:46:54 +0700

    Bài tập Palindrome

--- algorithms/exercise.js
+++ algorithms/exercise.js
@@ -9,3 +9,22 @@ function arrayReplace(array, elementToReplace, newElement) {
     return array;
 }
 console.log(arrayReplace([1, 2, 3, 1, 1], 1, 5)); // [5,2,3,5,5]
+
+// 2. Palindrome
+// aaBAA -> AABaa -> Palindrome
+// aaBAc -> cABaa -> not Palindrome
+function isPalindrome(str) {
+    const newStr = str.toLowerCase();
+
+    // C1
+    // const reverseStr = newStr.split("").reverse().join("");
+    // C2
+    let reverseStr = "";
+    for (let i = newStr.length - 1; i >= 0; i--) {
+        reverseStr += newStr[i];
+    }
+
+    return newStr === reverseStr;
+}
+console.log(isPalindrome("aaBAA")); // true
+console.log(isPalindrome("cABaa")); // false
